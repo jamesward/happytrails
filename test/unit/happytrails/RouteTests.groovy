bc8eec45c9d4ead03dc946defd6c670e026d9d69
Matt Raible <matt@raibledesigns.com>
2012-05-16 12:20:43 -0600

    Added Gorm Tests

--- test/unit/happytrails/RouteTests.groovy
+++ test/unit/happytrails/RouteTests.groovy
@@ -11,7 +11,23 @@ import org.junit.*
 @TestFor(Route)
 class RouteTests {
 
-    void testSomething() {
-       fail "Implement me"
+    void testConstraints() {
+        def region = new Region(name: "Colorado")
+        def whiteRanch = new Route(name: "White Ranch", distance: 12.0, location: "Golden, CO", region: region)
+        mockForConstraintsTests(Route, [whiteRanch])
+
+        // validation should fail if required properties are null
+        def route = new Route()
+        assert !route.validate()
+        assert "nullable" == route.errors["name"]
+
+        // verify unique constraint
+        route = new Route(name: "White Ranch", distance: 3, location: "Golden, CO", region: region)
+        assert !route.validate()
+        assert "unique" == route.errors["name"]
+
+        // validation should pass
+        route = new Route(name: "Matthew Winters", distance: 3, location: "Morrison, CO", region: region)
+        assert route.validate()
     }
 }
