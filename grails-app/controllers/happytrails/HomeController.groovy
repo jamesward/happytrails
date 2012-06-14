1016258682ed0cadead135f484d7427efce65975
Matt Raible <matt@raibledesigns.com>
2012-06-14 00:50:35 -0600

    Sort comments by most recently created.

--- grails-app/controllers/happytrails/HomeController.groovy
+++ grails-app/controllers/happytrails/HomeController.groovy
@@ -6,6 +6,7 @@ class HomeController {
 
     def index() {
         params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [regions: Region.list(params), total: Region.count(), comments: Comment.list(max: 10)]
+        [regions: Region.list(params), total: Region.count(),
+         comments: Comment.list(max: 10, sort: 'dateCreated', order: 'desc')]
     }
 }
