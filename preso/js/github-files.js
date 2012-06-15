728cb31299717cdff04b590d105c14cfef10a755
Matt Raible <matt@raibledesigns.com>
2012-06-15 11:58:21 -0700

    Added first grails code example and link to source file.

--- preso/js/github-files.js
+++ preso/js/github-files.js
@@ -9,22 +9,39 @@ function getGithubFile(user, repo, sha, callback, startLineNum, endLineNum) {
         url:url,
         dataType:"jsonp",
         success:function (data) {
-            if (typeof data.data.content != "undefined") {
-                if (data.data.encoding == "base64") {
-                    var base64EncodedContent = data.data.content
-                    base64EncodedContent = base64EncodedContent.replace(/\n/g, "")
+            if (data.data.message == 'Not Found') {
+                callback("Source file not found.")
+            } else {
+                if (typeof data.data.content != "undefined") {
+                    if (data.data.encoding == "base64") {
+                        var base64EncodedContent = data.data.content
+                        base64EncodedContent = base64EncodedContent.replace(/\n/g, "")
 
-                    var content = window.atob(base64EncodedContent)
+                        var content = window.atob(base64EncodedContent)
 
-                    var contentArray = content.split("\n")
+                        var contentArray = content.split("\n")
 
-                    if (endLineNum == 0) {
-                        endLineNum = contentArray.length
+                        if (endLineNum == 0) {
+                            endLineNum = contentArray.length
+                        }
+
+                        callback(contentArray.slice(startLineNum - 1, endLineNum).join("\n"), data.data.url)
+
                     }
-
-                    callback(contentArray.slice(startLineNum - 1, endLineNum).join("\n"))
                 }
             }
         }
     });
+}
+
+function getSource(sha, callback, startLineNum, endLinNum) {
+    return getGithubFile("jamesward", "happytrails", sha, callback, startLineNum, endLinNum);
+}
+
+function linkToFile(selector, branch) {
+    var base = "https://github.com/jamesward/happytrails/blob/" + branch;
+    var filepath = $(selector + "-filename").text();
+    var link = base + "/" + filepath;
+
+    $(selector + "-filename").html($("<a/>").attr("href", link).addClass("roll").append(filepath));
 }
