5cf78adeea8cd9ad876095e6e45e7176b5dff0a5
James Ward <james@jamesward.com>
2020-03-04 09:50:43 -0700

    replace github api with raw access

--- preso/js/github-files.js
+++ preso/js/github-files.js
@@ -1,49 +1,29 @@
-function getGithubFile(user, repo, sha, callback, startLineNum, endLineNum) {
-    startLineNum = (typeof startLineNum == "undefined") ? 1 : startLineNum
-    endLineNum = (typeof endLineNum == "undefined") ? 0 : endLineNum
-
-    var url = "https://api.github.com/repos/" + user + "/" + repo + "/git/blobs/" + sha + "?client_id=703475a99fa5017d2921&client_secret=99ed6dc1edac3f3f5d5f300ac0d4d682ff641b3a"
+function getSrc(branch, path, callback, startLineNum, endLineNum) {
+    const url = "https://raw.githubusercontent.com/jamesward/happytrails/" + branch + "/" + path;
 
     $.ajax({
-        type:"GET",
-        url:url,
-        dataType:"jsonp",
-        success:function (data) {
-            if (data.data.message == 'Not Found') {
-                callback("Source file not found.")
-            } else {
-                if (typeof data.data.content != "undefined") {
-                    if (data.data.encoding == "base64") {
-                        var base64EncodedContent = data.data.content
-                        base64EncodedContent = base64EncodedContent.replace(/\n/g, "")
-
-                        var content = window.atob(base64EncodedContent)
-
-                        var contentArray = content.split("\n")
-
-                        if (endLineNum == 0) {
-                            endLineNum = contentArray.length
-                        }
-
-                        callback(contentArray.slice(startLineNum - 1, endLineNum).join("\n"), data.data.url)
-
-                    }
-                }
-            }
+        type: "GET",
+        url: url,
+        success: function (data) {
+            const lines = data.split('\n').slice(startLineNum - 1, endLineNum).join('\n');
+            callback(lines);
         }
     });
 }
 
-function getSource(sha, callback, startLineNum, endLinNum) {
-    return getGithubFile("jamesward", "happytrails", sha, callback, startLineNum, endLinNum);
-}
-
-function linkToFile(selector, branch, lineNo) {
+function linkToFile(selector, branch, startLineNum, endLineNum) {
     var base = "https://github.com/jamesward/happytrails/blob/" + branch;
     var filepath = $(selector + "-filename").text();
     var link = base + "/" + filepath;
-    if (lineNo) {
-        link += "#L" + lineNo;
+    if (startLineNum !== undefined) {
+        link += "#L" + startLineNum;
+
+        if (endLineNum !== undefined) {
+            link += "-L" + endLineNum;
+        }
+        else {
+            link += "-L10000";
+        }
     }
 
     $(selector + "-filename").html($("<a/>").attr("href", link).addClass("roll").append(filepath));
